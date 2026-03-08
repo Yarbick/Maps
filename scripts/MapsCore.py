0680c38a53059c4f20dc8b06a5cbd25a3981602a
Yarbick <yarbilovilyas@gmail.com>
2026-03-09 00:27:36 +0300

    Движение карты
    MapsCore.py:
    1. Добавлена возможность двигать карту с помощью стрелок

--- scripts/MapsCore.py
+++ scripts/MapsCore.py
@@ -7,18 +7,21 @@ import arcade
 # Работа с API
 from scripts.MapsAPI import API
 
-# Временные константы (будут получаться при работе приложения в будущем)
-LL, SPN = "37.617698,55.755864", "1,1"
-
 
 class Maps(arcade.Window, API):
     """Основной класс приложения"""
 
     def setup(self):
         # Масштаб карты
-        self.min_map_zoom: int = 0
-        self.max_map_zoom: int = 21
-        self.map_zoom: int = 12
+        self.min_map_z, self.max_map_z = 0, 21
+        self.delta_map_z_coef = 1
+        self.map_z = 12
+
+        # Движение карты
+        self.min_map_long, self.max_map_long = -180, 180
+        self.min_map_lat, self.max_map_lat = -90, 90
+        self.delta_map_ll_coef = 250
+        self.map_long, self.map_lat = 37.617698, 55.755864
 
         # Получение карты
         self.get_map_image()
@@ -27,7 +30,9 @@ class Maps(arcade.Window, API):
         """Получение и запись изображения карты из кэша в атрибут класса"""
 
         self.map_image: arcade.Texture = API.StaticMaps.get_map_image(
-            Cacher.CACHE_PARAMS["MAP_IMAGE_FILE_NAME"], ll=LL, z=str(self.map_zoom)
+            Cacher.CACHE_PARAMS["MAP_IMAGE_FILE_NAME"],
+            ll=f"{self.map_long},{self.map_lat}",
+            z=str(self.map_z)
         )
 
     # Методы arcade.Window
@@ -40,9 +45,33 @@ class Maps(arcade.Window, API):
         )
 
     def on_key_press(self, key: int, modifiers: int) -> None:
+        map_changed = False  # Флаг для обновления карты только при наличии изменений
+
         # Зум карты
-        if key == arcade.key.PAGEUP or key == arcade.key.PAGEDOWN:
-            delta_zoom: int = 1 if key == arcade.key.PAGEUP else -1
-            if self.min_map_zoom <= self.map_zoom + delta_zoom <= self.max_map_zoom:
-                self.map_zoom += delta_zoom
-                self.get_map_image()
+        if key == key == arcade.key.PAGEUP and self.map_z + self.delta_map_z_coef <= self.max_map_z:
+            self.map_z += self.delta_map_z_coef
+            map_changed = True
+        if key == key == arcade.key.PAGEDOWN and self.map_z - self.delta_map_z_coef >= self.min_map_z:
+            self.map_z -= self.delta_map_z_coef
+            map_changed = True
+
+        # Движение карты
+        delta_map_long = self.delta_map_ll_coef * (2 ** -self.map_z)
+        if key == arcade.key.LEFT and self.map_long - delta_map_long >= self.min_map_long:
+            self.map_long -= delta_map_long
+            map_changed = True
+        if key == arcade.key.RIGHT and self.map_long + delta_map_long <= self.max_map_long:
+            self.map_long += delta_map_long
+            map_changed = True
+
+        delta_map_lat = delta_map_long * 0.5
+        if key == arcade.key.DOWN and self.map_lat - delta_map_lat >= self.min_map_lat:
+            self.map_lat -= delta_map_lat
+            map_changed = True
+        if key == arcade.key.UP and self.map_lat + delta_map_lat <= self.max_map_lat:
+            self.map_lat += delta_map_lat
+            map_changed = True
+
+        # Обновление карты
+        if map_changed:
+            self.get_map_image()
